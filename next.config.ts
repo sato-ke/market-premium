9d894ac4c2b43b36134d4b647983b92c602994b8
sato-keichi <sato_keichi@cybc.jp>
2025-01-21 14:09:48 +0900

    assetPrefixを追加

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/market-premium",
+  assetPrefix: "/market-premium/",
   images: {
     unoptimized: true,
   },
